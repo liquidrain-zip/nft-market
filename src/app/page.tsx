35dc640a967c666da22aa7666e65dc0d4e1d8397
Shabir Mohd Bashir <m.shabirmb@gmail.com>
2024-05-01 19:53:42 +0800

    update home, middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <></>;
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/dashboard");
+    },
+  });
+  const user = session?.user;
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Welcome back {user?.name}!
+          </h1>
+        </div>
+      </header>
+    </>
+  );
 }
